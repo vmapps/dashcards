dc111493b96c5d6a138e465c3c24a2a6b16606d4
VM Apps <31423375+vmapps@users.noreply.github.com>
2019-06-10 13:17:25 +0200

    add finance plugin and fix sample template

---
+++ plugins/finance.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import json
+import urllib.request
+
+name = 'Finance Quote plugin'
+version = '0.1'
+url = 'https://githib.com/vmapps/'
+author = 'VMapps'
+contact	= '31423375+vmapps@users.noreply.github.com'
+description = 'Sample plugin for demo'
+
+def test():
+	return name + ' - v' + version
+
+def run(args):
+	
+	# for demo purposes, this service relies on Financial Modeling Prep services
+	# check at https://financialmodelingprep.com/developer/docs/
+
+	url = 'https://financialmodelingprep.com/api/v3/stock/real-time-price/' + args[1]
+
+	jobj = urllib.request.urlopen(url)
+	data = json.loads( jobj.read() )
+
+	data['price'] = '%.2f' % data['price']
+	data['name'] = args[0] 
+
+	return data
+	
+	
